5caa3e07d83c51905f337ff4d08f3d554e0e9e34
e.vrublevskaya <e.vrublevskaya@visionlabs.ru>
2024-07-16 15:25:50 +0300

    Readme added

--- tasks/views.py
+++ tasks/views.py
@@ -1,88 +1,11 @@
-# from django.http import HttpResponseNotFound
-# from django.shortcuts import render, redirect
-# from django.contrib.auth import login
-# from django.contrib.auth.decorators import login_required
-# from .forms import UserRegistrationForm, TaskForm, UserLoginForm
-# from .models import Task
-#
-#
-# def register(request):
-#     if request.method == 'POST':
-#         form = UserRegistrationForm(request.POST)
-#         if form.is_valid():
-#             new_user = form.save(commit=False)
-#             new_user.set_password(form.cleaned_data['password'])
-#             new_user.save()
-#             return redirect('login')
-#     else:
-#         form = UserRegistrationForm()
-#     return render(request, 'tasks/register.html', {'form': form})
-#
-#
-# def user_login(request):
-#     if request.method == 'POST':
-#         form = UserLoginForm(data=request.POST)
-#         if form.is_valid():
-#             user = form.get_user()
-#             login(request, user)
-#             return redirect('task_list')
-#     else:
-#         form = UserLoginForm()
-#     return render(request, 'tasks/login.html', {'form': form})
-#
-#
-# @login_required
-# def task_list(request):
-#     tasks = Task.objects.all()
-#     return render(request, 'tasks/task_list.html', {'tasks': tasks})
-#
-#
-# @login_required
-# def task_create(request):
-#     if request.method == 'POST':
-#         form = TaskForm(request.POST)
-#         if form.is_valid():
-#             task = form.save(commit=False)
-#             task.user = request.user
-#             task.save()
-#             return redirect('tasks')
-#     else:
-#         form = TaskForm()
-#     return render(request, 'tasks/task_form.html', {'form': form})
-#
-#
-# @login_required
-# def task_edit(request, pk):
-#     task = Task.objects.get(pk=pk)
-#     if request.method == 'POST':
-#         form = TaskForm(request.POST, instance=task)
-#         if form.is_valid():
-#             form.save()
-#             return redirect('tasks')
-#     else:
-#         form = TaskForm(instance=task)
-#     return render(request, 'tasks/task_form.html', {'form': form})
-#
-#
-# @login_required
-# def task_delete(request, pk):
-#     task = Task.objects.get(pk=pk)
-#     if request.method == 'POST':
-#         task.delete()
-#         return redirect('tasks')
-#     return render(request, 'tasks/task_confirm_delete.html', {'task': task})
-#
-#
-# def page_not_found(request, exception):
-#     return HttpResponseNotFound("<h1>Запрашиваемая страница не найдена</h1>")
 from django.contrib.auth import authenticate, login
 from django.contrib.auth import logout
-from django.contrib.auth.forms import AuthenticationForm
 from django.http import HttpResponseNotFound
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .forms import UserRegistrationForm, TaskForm, UserLoginForm
 from .models import Task
+
 
 def register(request):
     if request.method == 'POST':
@@ -96,6 +19,7 @@ def register(request):
         form = UserRegistrationForm()
     return render(request, 'tasks/register.html', {'form': form})
 
+
 def user_login(request):
     if request.method == 'POST':
         form = UserLoginForm(data=request.POST)
@@ -106,6 +30,7 @@ def user_login(request):
     else:
         form = UserLoginForm()
     return render(request, 'tasks/login.html', {'form': form})
+
 
 def task_list(request):
     if not request.user.is_authenticated:
@@ -131,6 +56,7 @@ def task_list(request):
         tasks = Task.objects.filter(user=request.user)
         return render(request, 'tasks/task_list.html', {'tasks': tasks})
 
+
 @login_required
 def task_create(request):
     if request.method == 'POST':
@@ -144,6 +70,7 @@ def task_create(request):
         form = TaskForm()
     return render(request, 'tasks/task_form.html', {'form': form})
 
+
 @login_required
 def task_edit(request, pk):
     task = Task.objects.get(pk=pk)
@@ -156,6 +83,7 @@ def task_edit(request, pk):
         form = TaskForm(instance=task)
     return render(request, 'tasks/task_form.html', {'form': form})
 
+
 @login_required
 def task_delete(request, pk):
     task = Task.objects.get(pk=pk)
@@ -164,9 +92,11 @@ def task_delete(request, pk):
         return redirect('tasks')
     return render(request, 'tasks/task_confirm_delete.html', {'task': task})
 
+
 def user_logout(request):
     logout(request)
     return redirect('task_list')
+
 
 def page_not_found(request, exception):
     return HttpResponseNotFound("<h1>Запрашиваемая страница не найдена</h1>")
